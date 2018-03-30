51709501f263d5ce6eed67ce22ef57f733f16820
hongjie <32968210@qq.com>
2018-03-30 16:01:08 +0800

    auth

--- src/models/login.js
+++ src/models/login.js
@@ -25,6 +25,7 @@ export default {
                     type: 'saveToken',
                     payload: response.data.token,
                 });
+                setAuthority('admin');
                 reloadAuthorized();
                 yield put(routerRedux.push('/'));
             }
--- src/router.js
+++ src/router.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { routerRedux, Route, Switch } from 'dva/router';
+import { routerRedux, Route, Switch, Redirect } from 'dva/router';
 import { LocaleProvider, Spin } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 import dynamic from 'dva/dynamic';
 import { getRouterData } from './common/router';
 import Authorized from './utils/Authorized';
 import styles from './index.less';
+import * as storage from './utils/storage';
 
 const { ConnectedRouter } = routerRedux;
 const { AuthorizedRoute } = Authorized;
 dynamic.setDefaultLoadingComponent(() => {
     return <Spin size="large" className={styles.globalSpin} />;
 });
+
+/**
+ * 验证登录
+ */
+const checkAuth = () => {
+    const user = storage.getItem(storage.USER);
+    return user;
+};
 
 function RouterConfig({ history, app }) {
     const routerData = getRouterData(app);
@@ -27,7 +36,10 @@ function RouterConfig({ history, app }) {
                     />
                     <AuthorizedRoute
                         path="/"
-                        render={props => <BasicLayout {...props} />}
+                        // render={props => <BasicLayout {...props} />}
+                        render={(props) => {
+                            return checkAuth() ? <BasicLayout {...props} /> : <Redirect to={{ pathname: '/user/login' }} />;
+                        }}
                         authority={['admin', 'user']}
                         redirectPath="/user/login"
                     />
