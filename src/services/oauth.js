c1774501175642fc802845fbae7ea4c62be2a0cd
hongjie <32968210@qq.com>
2018-03-30 18:27:30 +0800

    fixed a bug

--- src/services/oauth.js
+++ src/services/oauth.js
@@ -2,9 +2,8 @@ import { stringify } from 'qs';
 import { token } from '../utils/request';
 
 export async function login(params) {
-    // return token('/oauth2/access_token', `grant_type=password&${stringify(params)}`);
     return token(
-        'http://127.0.0.1:7001/oauth2/access_token',
+        '/oauth2/access_token',
         `grant_type=password&${stringify(params)}`
     );
 }
