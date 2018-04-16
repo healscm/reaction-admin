696b371ba93960a2bc54b2c5f4f3a25f4500be5c
hongjie <32968210@qq.com>
2018-04-16 10:23:46 +0800

    小程序路径改成az的路径

--- src/common/menu.js
+++ src/common/menu.js
@@ -117,6 +117,33 @@ import { isUrl } from '../utils/utils';
 
 const menuData = [
     {
+        name: 'AZ',
+        icon: 'api',
+        path: 'az',
+        children: [
+            {
+                name: '用户列表',
+                path: 'user-list',
+                icon: 'user',
+            },
+            {
+                name: '症状类别列表',
+                path: 'category-list',
+                icon: 'appstore-o',
+            },
+            {
+                name: '症状列表',
+                path: 'reaction-list',
+                icon: 'bars',
+            },
+            {
+                name: '患友经验列表',
+                path: 'exp-list',
+                icon: 'exception',
+            },
+        ],
+    },
+    {
         name: '小程序',
         icon: 'wechat',
         path: 'mini-program',
@@ -129,17 +156,17 @@ const menuData = [
             {
                 name: '症状类别列表',
                 path: 'category-list',
-                icon: 'user',
+                icon: 'appstore-o',
             },
             {
                 name: '症状列表',
                 path: 'reaction-list',
-                icon: 'user',
+                icon: 'bars',
             },
             {
                 name: '患友经验列表',
                 path: 'exp-list',
-                icon: 'user',
+                icon: 'exception',
             },
         ],
     },
